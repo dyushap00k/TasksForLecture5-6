452423d2162969b18493e9c142a0fbedbf16e979
dyushap00k <117974589+dyushap00k@users.noreply.github.com>
2022-12-12 20:22:04 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,9 +9,7 @@ import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) throws JAXBException, IOException, BuildException, InvocationTargetException, NoSuchMethodException, IllegalAccessException, InstantiationException {
-//        Task1.doCalculateJsonToXml(new File("src/main/resources/json_fines"));
-//        Task1.doCalculateXmlToJson(new File("src/main/resources/xml_fines"));
-        ExampleClass1 exampleClass1 = UtilClass.toBuild(ExampleClass1.class, Path.of("src/main/resources/properties1.properties"));
-        System.out.println(exampleClass1);
+        Task1.doCalculateJsonToXml(new File("src/main/resources/json_fines"));
+        Task1.doCalculateXmlToJson(new File("src/main/resources/xml_fines"));
     }
 }
